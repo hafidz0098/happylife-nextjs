cb2e9ba976561baaa95f0aaf89a02701b5a8f90e
hafidz0098 <hafidznak123@gmail.com>
2023-10-10 01:25:28 +0700

    add apple icon

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,15 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" type="image/png" sizes="16x16" href="/icon-256x256.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icon-384x384.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icon-512x512.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
 
